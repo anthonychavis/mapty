977fab3d4dbd676c4d09eaf5c74387e473e276f5
Anthony C <gitanthony@yahoo.com>
2022-05-22 20:37:32 -0400

    workout rendered to list, marker popup updated, form hidden

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ class Workout {
         this.distance = distance; // km
         this.duration = duration; // mins
     }
+
+    // private method
+    _setDescription() {
+        // prettier-ignore
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        this.description = `${this.type[0].toUpperCase()}${this.type.slice(
+            1
+        )} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
 }
 
 // child Classes for Workout details
@@ -23,6 +33,7 @@ class Running extends Workout {
         this.cadence = cadence;
 
         this.calcPace(); // ok to call any code in constructor
+        this._setDescription(); // method defined in parent Class, but uses a property/field in child Class. So, called in child constructor
     }
 
     // Public interface - Public method
@@ -36,11 +47,13 @@ class Running extends Workout {
 class Cycling extends Workout {
     // public field
     type = 'cycling';
+
     constructor(coords, distance, duration, elevationGain) {
         super(coords, distance, duration);
         this.elevationGain = elevationGain;
 
         this.calcSpeed();
+        this._setDescription(); // method defined in parent Class, but uses a property/field in child Class. So, called in child constructor
     }
 
     // Public interface - Public method
@@ -59,8 +72,6 @@ class Cycling extends Workout {
 /////////////////////////////////////////////////////////
 // APPLICATION ARCHITECTURE
 // these global vars could go into the Class App as fields
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -82,7 +93,7 @@ class App {
     constructor() {
         this._getPosition();
 
-        // top level handlers:
+        // (formerly top level handlers)
         // form event handler to submit form & render marker. (unrelated to geolocation. So, built outside the getCurrentPosition() method).
         form.addEventListener('submit', this._newWorkout.bind(this)); // bind method returns new fxn
 
@@ -106,7 +117,7 @@ class App {
     _loadMap(position) {
         // console.log(position); // check object
 
-        // take position out
+        // take coords from position object
         // const latitude = position.coords.latitude;
         const { latitude } = position.coords; // destructuring
         const { longitude } = position.coords; // destructuring
@@ -118,7 +129,7 @@ class App {
 
         const coords = [latitude, longitude];
 
-        // from Leaflet, then edited. gives location
+        // from Leaflet, then edited. gives user location
         this.#map = L.map('map').setView(coords, 13); // map string = id of element for rendering
         // L = Leaflet namespace (like Intl) & global variable ♦
         // 13 = zoom value
@@ -140,6 +151,20 @@ class App {
         // form
         form.classList.remove('hidden');
         inputDistance.focus(); // add cursor
+    }
+
+    _hideForm() {
+        // clear input fields after submitting
+        inputDistance.value =
+            inputDuration.value =
+            inputCadence.value =
+            inputElevation.value =
+                '';
+
+        // hide form
+        form.style.display = 'none'; // remove transition/animation ♦
+        form.classList.add('hidden');
+        setTimeout(() => (form.style.display = 'grid'), 1000); // return display value to origianal value ♦
     }
 
     _toggleElevationField() {
@@ -172,7 +197,7 @@ class App {
         if (type === 'running') {
             const cadence = +inputCadence.value;
 
-            // Check if data valid
+            // Check if data valid - guard clause
             if (
                 !validInputs(distance, duration, cadence) ||
                 !allPositive(distance, duration, cadence)
@@ -186,7 +211,7 @@ class App {
         if (type === 'cycling') {
             const elevation = +inputElevation.value;
 
-            // Check if data valid
+            // Check if data valid - guard clause
             if (
                 !validInputs(distance, duration, elevation) ||
                 !allPositive(distance, duration) // elevation could be neg
@@ -204,15 +229,10 @@ class App {
         this._rederWorkoutMarker(workout);
 
         // Render workout on the list
+        this._renderWorkout(workout);
 
         // Hide form + clear input fields
-
-        // clear input fields after submitting
-        inputDistance.value =
-            inputDuration.value =
-            inputCadence.value =
-            inputElevation.value =
-                '';
+        this._hideForm();
     }
 
     _rederWorkoutMarker(workout) {
@@ -228,8 +248,69 @@ class App {
                     className: `${workout.type}-popup`,
                 })
             )
-            .setPopupContent(workout.type) // inherited method
+            .setPopupContent(
+                `${workout.type === 'running' ? '🏃' : '🚴'} ${
+                    workout.description
+                }`
+            ) // inherited method
             .openPopup(); // Read Leaflet documentation to remember how this chaining works
+    }
+
+    _renderWorkout(workout) {
+        let html = `
+            <li class="workout workout--${workout.type}" data-id="${
+            workout.id
+        }">
+                <h2 class="workout__title">${workout.description}</h2>
+                <div class="workout__details">
+                    <span class="workout__icon">${
+                        workout.type === 'running' ? '🏃' : '🚴'
+                    }</span>
+                    <span class="workout__value">${workout.duration}</span>
+                    <span class="workout__unit">km</span>
+                </div>
+                <div class="workout__details">
+                    <span class="workout__icon">⏱</span>
+                    <span class="workout__value">${workout.duration}</span>
+                    <span class="workout__unit">min</span>
+                </div>
+            `; // data attribute used to build a bridge b/t the ui & data in the application
+
+        if (workout.type === 'running')
+            html += `
+                <div class="workout__details">
+                    <span class="workout__icon">⚡️</span>
+                    <span class="workout__value">${workout.pace.toFixed(
+                        1
+                    )}</span>
+                    <span class="workout__unit">min/km</span>
+                </div>
+                <div class="workout__details">
+                    <span class="workout__icon">🦶🏼</span>
+                    <span class="workout__value">${workout.cadence}</span>
+                    <span class="workout__unit">spm</span>
+                </div>
+            </li>
+            `;
+
+        if (workout.type === 'cycling')
+            html += `
+                <div class="workout__details">
+                    <span class="workout__icon">⚡️</span>
+                    <span class="workout__value">${workout.speed.toFixed(
+                        1
+                    )}</span>
+                    <span class="workout__unit">km/h</span>
+                </div>
+                <div class="workout__details">
+                    <span class="workout__icon">⛰</span>
+                    <span class="workout__value">${workout.elevationGain}</span>
+                    <span class="workout__unit">m</span>
+                </div>
+            </li>
+            `;
+
+        form.insertAdjacentHTML('afterend', html);
     }
 }
 
@@ -237,6 +318,3 @@ class App {
 const app = new App(); // created on pg load b/c it's in the top level scope. Constructor executed on Instance creation.
 
 // ♠ NOTE at the moment, if cycling is left as the inputType before pg refresh, the cadence/elevation input field is broken [will cause a problem when storing data] ♠
-
-// hide generic form after submitting
-// add submitted form/data to list
